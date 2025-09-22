4022ccff49691df420e2da351fc7f8c5e4cc403d
Aleks <o.matyka@andersenlab.com>
2025-09-22 21:19:25 +0300

    some b

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import AppRouter from '@/router/router'
 import { useEffect } from 'react'
 import { useAuth } from '@/shared/hooks/useAuthStore.ts'
 
-// 1
+// my new changes
 export default function App() {
   const { checkAuth } = useAuth()
 
